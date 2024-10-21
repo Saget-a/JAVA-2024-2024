77021a49a13dd841e72b80de1c13a3bb60fbae6f
Saget <132357928+Saget-a@users.noreply.github.com>
2024-10-22 01:46:00 +0300

    modified:   Lab4.java

--- Lab4.java
+++ Lab4.java
@@ -2,42 +2,56 @@ public class Lab4 {
 // собственно завдання 21 51 67
 // для перевірки буду використовувати масив A^2 до 12 з чергуванням знаку : -2 9 -16 25 -36 49 -64 81 -100 121 -144
 
-// завдання 21 
-// Знайти добуток модулів найбільшого та найменшого елементів масиву
-
-// Знайти суму від’ємних елементів, індекс яких кратний 2
-
 // Задано масиви А(n) та В(n). Сформувати масив С(2*n), елементами якого є
 // елементи масивів A(n) та B(n), записані через один (a1, b1, a2, b2, a3, b3, ...)
 
-    public static void task21(String[] args) {
-        int[] arr = {2, 9, -16, 25, -36, 49, -64, 81, -100, 121, -144};
+// завдання 21 
+// Знайти добуток модулів найбільшого та найменшого елементів масиву
+    public static double task21(int[] array) {
         
-        // Знайдемо максимальний і мінімальний елементи
-        int max = arr[0];
-        int min = arr[0];
-        
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
+        int max = array[0];
+        int min = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
             }
-            if (arr[i] < min) {
-                min = arr[i];
+            if (array[i] < min) {
+                min = array[i];
             }
         }
-        
-        // Обчислимо модулі
-        int absMax = Math.abs(max);
-        int absMin = Math.abs(min);
-        
-        // Знайдемо добуток
-        int product = absMax * absMin;
-        
-        // Виведемо результат
-        System.out.println("Добуток модулів найбільшого та найменшого елементів: " + product);
+
+        int maxAbs = Math.abs(max);
+        int minAbs = Math.abs(min);
+
+        int product = maxAbs * minAbs;
+        return product;
     }
    
+// завдання 51
+// Знайти суму від’ємних елементів, індекс яких кратний 2
+    public static double task51() {
+        int[] array = {-2, 9, -16, 25, -36, 49, -64, 81, -100, 121, -144};
+
+        double sum = 0;
+
+        // Проходимо по елементах з індексами, що кратні 2
+        for (int i = 0; i < array.length; i += 2) {
+            if (array[i] < 0) {
+                sum += array[i];
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args ) {
-        int t = task21;
+        int[] testArray1 = {-2, 9, -16, 25, -36, 49, -64, 81, -100, 121, -144};
+        int[] testArray2 = {2, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144};
+        int[] testArray3 = {-2, -9, -16, -25, -36, -49, -64, -81, -100, -121, -144};
+
+        System.err.println("Чергування +-    >>>" + task21(testArray1));
+        System.err.println("Додатный массив  >>>" + task21(testArray2));
+        System.err.println("Від'ємний массив >>>" + task21(testArray3));
+
     }
 }
