d1d205bc26710a694deb494c3421777a7d1aadfb
Saget <132357928+Saget-a@users.noreply.github.com>
2024-10-22 02:49:22 +0300

    modified:   .gitignore 	modified:   Lab4.java

--- Lab4.java
+++ Lab4.java
@@ -1,9 +1,4 @@
 public class Lab4 {
-// собственно завдання 21 51 67
-// для перевірки буду використовувати масив A^2 до 12 з чергуванням знаку : -2 9 -16 25 -36 49 -64 81 -100 121 -144
-
-// Задано масиви А(n) та В(n). Сформувати масив С(2*n), елементами якого є
-// елементи масивів A(n) та B(n), записані через один (a1, b1, a2, b2, a3, b3, ...)
 
 // завдання 21 
 // Знайти добуток модулів найбільшого та найменшого елементів масиву
@@ -30,8 +25,7 @@ public class Lab4 {
    
 // завдання 51
 // Знайти суму від’ємних елементів, індекс яких кратний 2
-    public static double task51() {
-        int[] array = {-2, 9, -16, 25, -36, 49, -64, 81, -100, 121, -144};
+    public static double task51(int[] array) {
 
         double sum = 0;
 
@@ -43,15 +37,53 @@ public class Lab4 {
         }
         return sum;
     }
+// завдання 67
+// Задано масиви А(n) та В(n). Сформувати масив С(2*n), елементами якого є
+// елементи масивів A(n) та B(n), записані через один (a1, b1, a2, b2, a3, b3, ...)
 
+    public static int[] task67(int[] A, int[] B) {
+
+        // Перевірка, що масиви однакової довжини
+        if (A.length != B.length) {
+            System.out.println("ERROR 67.01");
+        }
+    
+        // Створення масиву C
+        int n = A.length;
+        int[] C = new int[2 * n];
+    
+        // Заповнення масиву C елементами з A та B
+        for (int i = 0; i < n; i++) {
+            C[2 * i] = A[i];       // елемент з A
+            C[2 * i + 1] = B[i];   // елемент з B
+        }
+    return C;
+    }
+
+    
     public static void main(String[] args ) {
         int[] testArray1 = {-2, 9, -16, 25, -36, 49, -64, 81, -100, 121, -144};
         int[] testArray2 = {2, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144};
         int[] testArray3 = {-2, -9, -16, -25, -36, -49, -64, -81, -100, -121, -144};
+        
+        int[] A = {9, 25, 49, 81, 121};
+        int[] B = {2, 16, 36, 64, 100};
 
-        System.err.println("Чергування +-    >>>" + task21(testArray1));
-        System.err.println("Додатный массив  >>>" + task21(testArray2));
-        System.err.println("Від'ємний массив >>>" + task21(testArray3));
+        System.err.println("тест завдання 21");
+        System.err.println("Чергування +-    >>> " + task21(testArray1));
+        System.err.println("Додатный массив  >>> " + task21(testArray2));
+        System.err.println("Від'ємний массив >>> " + task21(testArray3) + "\n");
 
+        System.err.println("тест завдання 51");
+        System.err.println("Чергування +-    >>> " + task51(testArray1));
+        System.err.println("Додатный массив  >>> " + task51(testArray2));
+        System.err.println("Від'ємний массив >>> " + task51(testArray3) + "\n");
+
+        int [] result67 = task67(A,B);
+        
+        System.out.print("З'єднання масиивів A & B: ");
+        for (int value : result67) {
+            System.out.print(value + " ");
+        }
     }
 }
